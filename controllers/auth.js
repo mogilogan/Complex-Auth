2659241454888eafc1999d60d28336bab6f3da3d
Moginder <mogiegan@gmail.com>
2022-03-01 23:36:09 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
-exports.register = (req,res, next) =>{
-    res.send("Register Route");
+const User = require('../models/User.js');
+
+
+exports.register = async(req,res, next) =>{
+    const {username , email, password} = req.body;
+try {
+    const user = await User.create({
+        username, email, password
+    });
+    res.status(200).json({
+        success: true,
+        user
+    });
+} catch (error) {
+    res.status(500).json({
+        success: false,
+        error: error.message,
+     
+    });
+}
+
 };
 
 exports.login= (req,res, next) =>{
