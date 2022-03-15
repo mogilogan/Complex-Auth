6e45e647afe36c5880275e05af4aa90ecbd23798
mogilogan <mogiegan@gamil.com>
2022-03-15 23:42:35 +0530

    added user model

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config({path: "./config.env"});
 const express = require('express');
+const connectDB = require('./config/db.js');
+
+connectDB();
 
 const app = express();
 
@@ -10,4 +13,9 @@ app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
+const server = app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
+
+process.on("unhandledRejection", (err,promise)=>{
+    console.log(`Logged Error: ${err}`);
+    server.close(()=> process.exit(1))
+} )
