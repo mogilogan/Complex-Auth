ceac3c2e3b24559289839c98a91bbaa3a3817d70
mogilogan <mogiegan@gamil.com>
2022-03-17 01:06:42 +0530

    login control added

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,8 +19,33 @@ try {
 }
 };
 
-exports.login= (req,res, next) =>{
-    res.send("login Route");
+exports.login= async (req,res, next) =>{
+    const { email, password} =  req.body;
+
+    if(!email || !password){
+        res.status(400).json({success : false,error:"Provide email and password"})
+    }
+
+    try {
+        const user = await User.findOne({email}).select("+password");
+
+        if(!user){
+           return res.status(404).json({success:false,error:"Invalid credentials"})
+        }
+
+        const isMatch = await user.matchPasswords(password);
+
+        if(!isMatch){
+            return res.status(404).json({success: false,error:"Invalid credentials"})
+        }
+
+        res.status(200).json({
+            success: true,
+            token: "dsfsdfsd4f",
+        });
+    } catch (error) {
+        res.status(500).json({success: false,error: error.message})
+    }
 };
 
 exports.forgotpassword = (req,res, next) =>{
