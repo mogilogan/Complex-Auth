bce5a2c215381c1ec2eca1edf0819f044981c854
mogilogan <mogiegan@gamil.com>
2022-03-16 23:59:32 +0530

    correct again

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
@@ -10,17 +9,4 @@ const connectDB = async () => {
   console.log("MongoDB Connected");
 };
 
-=======
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  console.log("MongoDB Connected");
-};
-
->>>>>>> 8f1e5afdc2a84b6cf297d1fe6f039432ceaefaa8
 module.exports = connectDB;
